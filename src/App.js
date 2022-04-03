49da2e9a915c7d415616a4f5c70007918d40dae0
unknown <linh-cat>
2022-04-03 12:11:47 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import { useTranslation } from "react-i18next";
+
+function HeaderComponent() {
+  const { t, i18n } = useTranslation('common');
+  return <h1>{t('welcome.title')}</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback="loading">
+      <div className="App">
+        <HeaderComponent />
+      </div>
+    </Suspense>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+i18next.init({
+  interpolation: { escapeValue: false },  // React already does escaping
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
